71e1eaec8fc6c45f3705558a027fd28a6c808544
Glitch (bustling-calm-meteorite) <none>
2021-11-03 08:29:33 +0000

    💷💿 Checkpoint ./commands/music/bass.js:5173917/2300 ./commands/music/clearqueue.js:5173917/218 ./commands/music/filter.js:5173917/2078

--- commands/music/clearqueue.js
+++ commands/music/clearqueue.js
@@ -4,13 +4,12 @@ module.exports = {
   description: "plays the previous music",
   run: async (client, message, args, player) => {
     let queue = player.getQueue(message.guild.id);
-    let track = queue.nowPlaying()
         if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`You are not in the same voice channel !`);
 
-        await player.clearQueue(message.guild.id);
+        await queue.clear();
 
-        message.channel.send(`Queue cleared! ✅`);
+        message.channel.send(`The queue has just been cleared 🗑️`);
     },
 };
